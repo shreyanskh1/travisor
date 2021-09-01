3536fc3bca5cbe9faf5a8cb45f701be376c9782d
Shreyans Khandelwal <shreyanskh1@gmail.com>
2021-09-01 19:19:37 +0530

    Revert "map component"
    This reverts commit 6ab7dd9fc925f46dd47bab1bde27dadd4dbe0ae3.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
-
-import useStyles from './styles';
 
 const Map = () => {
-  const classes = useStyles();
-  const isMobile = useMediaQuery('(max-width: 600px)');
-
-  const coordinates = { lat: 0, lng: 0 };
-
-  return (
-    <div className={classes.mapContainer}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyBN5bbuGBFcspH1OIpEamitBCVxrOiCLqs' }}
-        defaultCenter={coordinates}
-        center={coordinates}
-        defaultZoom={14}
-        margin={[50, 50, 50, 50]}
-        options={''}
-        onChange={''}
-        onChildClick={''}
-      ></GoogleMapReact>
-    </div>
-  );
+  return <div>Map</div>;
 };
 
 export default Map;
