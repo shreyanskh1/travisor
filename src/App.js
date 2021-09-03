469787e2cb990ddb83d3bab003ba8816950bc1c9
Shreyans Khandelwal <shreyanskh1@gmail.com>
2021-09-03 14:32:17 +0530

    changed default coordinates using geolocation

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Map from './components/Map/Map';
 const App = () => {
   const [places, setPlaces] = useState([]);
 
-  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState(null);
 
   useEffect(() => {
@@ -22,8 +22,6 @@ const App = () => {
 
   useEffect(() => {
     getPlacesData(bounds.sw, bounds.ne).then((data) => {
-      console.log(data);
-
       setPlaces(data);
     });
   }, [coordinates, bounds]);
